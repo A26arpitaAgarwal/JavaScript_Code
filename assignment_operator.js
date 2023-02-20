0f976df64f32dd2b0e999606b80647d439c3f8c9
Arpita Agarwal <120187471+A26arpitaAgarwal@users.noreply.github.com>
2023-02-20 18:28:52 +0530

    assignment_operators

---
+++ assignment_operator.js
@@ -0,0 +1,21 @@
+// Lets take some variables that is x and y
+x=10
+y=20
+
+x=y // Here, x is equal to 20
+y=x // Here, y is equal to 10
+
+//x = x+3
+x += 3; // x is now 23 
+
+x -= 3; // x is now 20
+
+x *= 3; // x is now 60
+
+//x = x/3
+x /= 3; // x is now 20
+
+x %= 3; // x is now 2
+
+//exponent **
+x **= 3; // x is now 8
