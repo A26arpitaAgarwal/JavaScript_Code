50cb662b4faded89b2ea44bd5ca03cdd5ea469fe
Arpita Agarwal <120187471+A26arpitaAgarwal@users.noreply.github.com>
2023-02-20 18:15:27 +0530

    comparison_operators

---
+++ comparison_operators.js
@@ -0,0 +1,17 @@
+//comparison operators
+const a = 5, b = 2, c = 'hello';
+
+// equal to operator
+console.log(a == 5);     // true
+console.log(b == '2');   // true
+console.log(c == 'Hello');  // false
+console.log(a != 2); // true
+console.log(b != 'Hello'); // true
+console.log(a > 2); // true
+console.log(a >= 5); //true
+console.log(b < 3); // true
+
+let age = prompt("enter your age:");
+age = Number.parseInt(n)
+let voteable = (age < 18) ? "Too young":"Old enough"; //ternary operator
+
