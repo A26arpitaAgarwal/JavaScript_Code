febe5249bd40a5f9ba93d392044e80a713aec497
Arpita Agarwal <120187471+A26arpitaAgarwal@users.noreply.github.com>
2023-02-20 19:00:22 +0530

    while and do while loop

---
+++ while_and_do_while.js
@@ -0,0 +1,18 @@
+// while loop printing numbers from 0 to 4
+// it will run until condition dont gets false
+let i = 0;
+
+while (i < 5) {
+  console.log("The value of i is ${i}.");
+  i++;
+}
+
+
+// do while loop
+// runs atleast one time if condition is not true
+let i = 0;
+
+do {
+  console.log("The value of i is ${i}.");
+  i++;
+} while (i < 5);
